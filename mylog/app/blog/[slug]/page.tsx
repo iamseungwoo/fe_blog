f74efafc61676ecb8bc41e441cb9b22c7849de23
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-10-12 14:58:21 +0000

    Feat: get series subpage

--- mylog/app/blog/[slug]/page.tsx
+++ mylog/app/blog/[slug]/page.tsx
@@ -1,5 +1,25 @@
-const Page = () => {
-  return <div>h1</div>;
+import { getAllPosts } from '@/app/lib/post';
+import { notFound } from 'next/navigation';
+
+const Page = async ({ params }: { params: { slug: string } }) => {
+  const slug = `/posts/${[decodeURI(params.slug)].join('/')}`;
+  const series = decodeURI(params.slug);
+  const seriesArticle: any[] = getAllPosts()
+    .map(post => post.slug)
+    .filter(ps => ps.split('/')[2] == series);
+
+  console.log(seriesArticle);
+  if (!seriesArticle.length) {
+    console.log(123);
+    return notFound();
+  }
+  return (
+    <>
+      {seriesArticle.map(el => (
+        <div>{el}</div>
+      ))}
+    </>
+  );
 };
 
 export default Page;
