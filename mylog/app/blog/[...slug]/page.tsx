7edb989973c0492180f48d1fc4ca88eed272ac84
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-10-13 14:56:38 +0000

    Fix: best bug fix is remove code

--- mylog/app/blog/[...slug]/page.tsx
+++ mylog/app/blog/[...slug]/page.tsx
@@ -2,12 +2,6 @@ import PostTitle from '@/app/component/PostPage/PostTitle';
 import { getAllPosts } from '@/app/lib/post';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { notFound } from 'next/navigation';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-const rehypeCodeTitles = require('rehype-code-titles');
-import rehypePrism from 'rehype-prism-plus';
-import rehypeSlug from 'rehype-slug';
-import remarkGfm from 'remark-gfm';
-import remarkToc from 'remark-toc';
 
 export default async function Page({ params }: { params: { slug: any } }) {
   const slug = `/posts/${[params.slug.join('/')].join('/')}`;
@@ -25,24 +19,6 @@ export default async function Page({ params }: { params: { slug: any } }) {
       <PostTitle post={post} />
       <MDXRemote
         source={markdown}
-        options={{
-          mdxOptions: {
-            remarkPlugins: [remarkToc, remarkGfm],
-            rehypePlugins: [
-              rehypeSlug,
-              rehypeCodeTitles,
-              rehypePrism,
-              [
-                rehypeAutolinkHeadings,
-                {
-                  properties: {
-                    className: ['anchor'],
-                  },
-                },
-              ],
-            ],
-          },
-        }}
       />
     </article>
   );
