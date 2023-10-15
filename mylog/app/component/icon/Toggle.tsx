c2f1005b6f1904f0b86d5f5c1663a235901308a6
seungwoo byeon <mongoose@yu.ac.kr>
2023-10-15 13:19:20 +0000

    Fix: default toggle button sun

--- mylog/app/component/icon/Toggle.tsx
+++ mylog/app/component/icon/Toggle.tsx
@@ -26,7 +26,7 @@ const Toggle = () => {
 
   return (
     <div onClick={onClickToggle}>
-      {theme === 'light' ? <BsSun /> : <BsMoon />}
+      {theme !== 'light' ? <BsSun /> : <BsMoon />}
     </div>
   );
 };
