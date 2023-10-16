89d32fa538029a4f25153445cd06c58673de33c6
seungwoo byeon <mongoose@yu.ac.kr>
2023-10-16 09:13:28 +0000

    Feat: real post test

--- mylog/app/article/[slug]/page.tsx
+++ mylog/app/article/[slug]/page.tsx
@@ -1,10 +1,16 @@
 import PostTitle from '@/app/component/PostPage/PostTitle';
 import Giscus from '@/app/component/global/Giscus';
 import { getAllPosts } from '@/app/lib/post';
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import { compileMDX } from 'next-mdx-remote/rsc';
 import { notFound } from 'next/navigation';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeCodeTitles from 'rehype-code-titles';
+import rehypePrism from 'rehype-prism-plus';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
+import remarkToc from 'remark-toc';
 
-const Page = ({ params }: { params: { slug: any } }) => {
+const Page = async ({ params }: { params: { slug: any } }) => {
   const slug = `/article/${[decodeURI(params.slug)].join('/')}`;
   const post = getAllPosts().find(post => post.slug === slug);
 
@@ -13,13 +19,35 @@ const Page = ({ params }: { params: { slug: any } }) => {
     return notFound();
   }
 
-  const markdown = post.content;
-  console.log(markdown);
+  const { content, frontmatter } = await compileMDX({
+    source: post.content,
+    options: {
+      parseFrontmatter: true,
+      mdxOptions: {
+        remarkPlugins: [remarkToc, remarkGfm],
+        rehypePlugins: [
+          [
+            rehypeSlug,
+            rehypeCodeTitles,
+            rehypePrism,
+            [
+              rehypeAutolinkHeadings,
+              {
+                properties: {
+                  className: ['anchor'],
+                },
+              },
+            ],
+          ],
+        ],
+      },
+    },
+  });
   return (
     <>
-      <article className="text-primary font-semibold">
-        <PostTitle post={post} />
-        <MDXRemote source={markdown} />
+      <PostTitle post={post} />
+      <article className="prose dark:prose-dark max-w-none my-12 prose-neutral font-spoqa dark:prose-dark">
+        {content}
       </article>
       <Giscus />
     </>
--- mylog/app/component/global/SubTitle.tsx
+++ mylog/app/component/global/SubTitle.tsx
@@ -2,7 +2,6 @@ const SubTitle = ({ subTitle }: { subTitle: string }) => {
   return (
     <h2 className="text-2xl font-bold tracking-tight md:text-4xl">
       {subTitle}
-      {/* <span className="ml-2 text-sm">(67)</span> */}
     </h2>
   );
 };
