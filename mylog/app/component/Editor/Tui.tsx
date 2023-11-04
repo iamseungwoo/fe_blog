2ef596e20051d9b0652152e02b988f88c8319cd8
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-11-04 14:43:36 +0000

    fix: toggle theme and switch tui editor theme

--- mylog/app/component/Editor/Tui.tsx
+++ mylog/app/component/Editor/Tui.tsx
@@ -1,12 +1,19 @@
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import { Editor } from '@toast-ui/react-editor';
 import axios from 'axios';
 import { useTheme } from 'next-themes';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import Prism from 'prismjs';
 
 type Props = {
   initValue: string;
 };
+
+const TUITHEME = 'toastui-editor-dark';
 
 const Tui = (props: Props) => {
   const toolbarItems = [
@@ -20,43 +27,41 @@ const Tui = (props: Props) => {
   ];
   const { resolvedTheme } = useTheme();
 
-  const onUploadImage = async (blob, callback) => {
-    // blob은 base64 인코딩된 이미지 파일
-    // formData에 담아 서버로 보내고, 서버에서는 s3에 이미지 저장후 s3에서 url을 받아 다시 프론트로 값 전송
-    const formData = new FormData()
-    formData.append('image', blob)
-    try {
-        const imageRes = await apiInstance.post('/image', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        })
-        const image_URL = imageRes.data.imageURL
-        setImage(image_URL)
-        // 글 화면에 이미지 띄우기
-        callback(image_URL, 'image')
-    } catch (e) {
-        console.error(e.response)
+  const theme = resolvedTheme;
+
+  const editorRef = useRef<Editor>(null);
+
+  useEffect(() => {
+    const editor = document.querySelector('.toastui-editor-defaultUI');
+    if (theme == 'light') {
+      editor?.classList.remove(TUITHEME);
+    } else {
+      editor?.classList.add(TUITHEME);
     }
-}
+  }, [theme]);
+
+  const handleSubmit = () => {
+    const content = editorRef.current?.getInstance().getHTML() || '';
+    console.log(content);
+  };
 
   return (
     <>
-      <Editor
-        initialValue={props.initValue}
-        previewStyle="vertical"
-        height="600px"
-        initialEditType="markdown"
-        useCommandShortcut={true}
-        toolbarItems={toolbarItems}
-        theme={resolvedTheme}
-        blob={{
-          addImageBlobHook: onAddImageBlob,
-        }}
-      />
+      <div id="editor">
+        <Editor
+          ref={editorRef}
+          initialValue={props.initValue}
+          previewStyle="vertical"
+          height="600px"
+          initialEditType="markdown"
+          useCommandShortcut={true}
+          toolbarItems={toolbarItems}
+          theme={theme}
+          plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
+        />
+      </div>
+      <button onClick={handleSubmit}>save</button>
     </>
   );
 };
-
-
 export default Tui;
