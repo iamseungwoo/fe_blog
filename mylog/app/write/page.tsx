7070b98ec61527649be4c1ba2e56774175bfce4d
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-11-18 14:23:14 +0000

    design: change editor 2

--- mylog/app/write/page.tsx
+++ mylog/app/write/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-const Editor = dynamic(() => import('../component/Editor/Editor'), {
+const Editor = dynamic(() => import('../component/Editor/Tui'), {
   ssr: false,
 });
 
